7ebd7d0a5c62559e8a8d278ea7565f888abdc4ac
fullvelocity1 <herdika.s1@gmail.com>
2022-06-06 15:31:22 +0700

    delet file ga kepake

--- ternaku/lib/helpers/readBookJson.dart
+++
@@ -1,7 +0,0 @@
-import 'package:ternaku/Items/buku.dart';
-import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart';
-import '../constants.dart';
-
-class readBookJson {}
